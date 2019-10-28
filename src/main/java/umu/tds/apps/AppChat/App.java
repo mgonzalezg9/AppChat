5909b3459609660d624b658839fcd5bd726c1b40
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-10-28 16:19:02 +0100

    Inicialización del proyecto

---
+++ src/main/java/umu/tds/apps/AppChat/App.java
@@ -0,0 +1,13 @@
+package umu.tds.apps.AppChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
