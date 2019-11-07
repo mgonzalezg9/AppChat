3c486faa1247c598b5175353a8840c68bb24f944
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-11-08 00:23:09 +0100

    Estructura principal de la ventana creada

--- src/main/java/umu/tds/apps/vistas/EstadisticasUso.java
+++ src/main/java/umu/tds/apps/vistas/EstadisticasUso.java
@@ -15,12 +15,27 @@ import javax.swing.JComboBox;
 import javax.swing.JButton;
 import java.awt.Dimension;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 class GraficaUso extends JFrame {
 	private JPanel contentPane;
-	
+
 	public GraficaUso() {
-		
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(EstadisticasUso.class.getResource("/umu/tds/apps/resources/icon.png")));
+		setTitle("Graphics");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 410, 213);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
 	}
 
 }
@@ -49,7 +64,8 @@ public class EstadisticasUso extends JFrame {
 	 * Create the frame.
 	 */
 	public EstadisticasUso() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(EstadisticasUso.class.getResource("/umu/tds/apps/resources/icon.png")));
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(EstadisticasUso.class.getResource("/umu/tds/apps/resources/icon.png")));
 		setTitle("Statistics");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 410, 213);
@@ -57,12 +73,12 @@ public class EstadisticasUso extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.insets = new Insets(0, 0, 5, 0);
@@ -70,26 +86,34 @@ public class EstadisticasUso extends JFrame {
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 0;
 		contentPane.add(panel, gbc_panel);
-		
+
 		JLabel lblGraphicType = new JLabel("Graphic type");
 		panel.add(lblGraphicType);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Default", "1", "2"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "Default", "1", "2" }));
 		comboBox.setPreferredSize(new Dimension(100, 20));
 		comboBox.setMinimumSize(new Dimension(78, 20));
 		panel.add(comboBox);
-		
+
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 1;
 		contentPane.add(panel_1, gbc_panel_1);
-		
+
 		JButton btnView = new JButton("VIEW");
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Nueva ventana con el grafico
+				setVisible(false);
+				GraficaUso grafica = new GraficaUso();
+				grafica.setVisible(true);
+			}
+		});
 		panel_1.add(btnView);
-		
+
 		JButton btnExport = new JButton("EXPORT");
 		panel_1.add(btnExport);
 	}
