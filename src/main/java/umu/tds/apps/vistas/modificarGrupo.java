26623ada5d49ca4098bf000f8135799435819ddc
AlfonsoLB <alfonsolopezbanos@gmail.com>
2019-11-08 19:50:25 +0100

    Vista para elegir grupo hecha

---
+++ src/main/java/umu/tds/apps/vistas/modificarGrupo.java
@@ -0,0 +1,71 @@
+package umu.tds.apps.vistas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class modificarGrupo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					modificarGrupo frame = new modificarGrupo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public modificarGrupo() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(modificarGrupo.class.getResource("/umu/tds/apps/resources/icon.png")));
+		setTitle("Choose group");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 449, 133);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{20, 0, 0, 20, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblChooseGroup = new JLabel("Choose group:");
+		GridBagConstraints gbc_lblChooseGroup = new GridBagConstraints();
+		gbc_lblChooseGroup.insets = new Insets(0, 0, 5, 0);
+		gbc_lblChooseGroup.gridx = 0;
+		gbc_lblChooseGroup.gridy = 1;
+		contentPane.add(lblChooseGroup, gbc_lblChooseGroup);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Juernes", "Cabina para TDS", "Futbol 16.30", "Spaghetti Coders"}));
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+		gbc_comboBox.gridx = 0;
+		gbc_comboBox.gridy = 2;
+		contentPane.add(comboBox, gbc_comboBox);
+	}
+
+}
