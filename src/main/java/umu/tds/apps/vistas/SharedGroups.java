1e3479af9d13e0cb2af02c4afce9b7b1d577f6e2
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-12-16 12:00:58 +0100

    Corregidos pequeños errores

--- src/main/java/umu/tds/apps/vistas/SharedGroups.java
+++ src/main/java/umu/tds/apps/vistas/SharedGroups.java
@@ -24,6 +24,7 @@ import umu.tds.apps.controlador.Controlador;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.util.List;
 
 import javax.swing.JList;
@@ -43,8 +44,8 @@ public class SharedGroups extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					//SharedGroups frame = new SharedGroups();
-					//frame.setVisible(true);
+					// SharedGroups frame = new SharedGroups();
+					// frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,40 +57,43 @@ public class SharedGroups extends JFrame {
 	 * Create the frame.
 	 */
 	public SharedGroups(IndividualContact contacto) {
+		setTitle("Shared groups");
+		setIconImage(
+				Toolkit.getDefaultToolkit().getImage(NewGroup.class.getResource("/umu/tds/apps/resources/icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
+
 		this.contact = contacto;
-		
+
 		contentPane = new JPanel();
 		contentPane.setBackground(MAIN_COLOR_LIGHT);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		final DefaultListModel<String> modelNameGroup = new DefaultListModel<>();
 		List<String> gruposCompartidos = Controlador.getInstancia().getNombresGrupo(contact);
 		for (int i = 0; i < gruposCompartidos.size(); i++)
 			modelNameGroup.add(i, gruposCompartidos.get(i));
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 0;
 		contentPane.add(scrollPane, gbc_scrollPane);
-		
+
 		JList<String> listSharedGroups = new JList<>(modelNameGroup);
 		listSharedGroups.setCellRenderer(createListRenderer());
 		scrollPane.setViewportView(listSharedGroups);
 		listSharedGroups.setBackground(MAIN_COLOR_LIGHT);
 	}
-	
+
 	private static ListCellRenderer<? super String> createListRenderer() {
 		return new DefaultListCellRenderer() {
 			private static final long serialVersionUID = 1L;
@@ -98,16 +102,16 @@ public class SharedGroups extends JFrame {
 			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
 					boolean cellHasFocus) {
 				String nombreGrupo = (String) value;
-				
+
 				JPanel panel = new JPanel();
 				panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 				GridBagLayout gbl_contentPane = new GridBagLayout();
-				gbl_contentPane.columnWidths = new int[]{0, 0};
-				gbl_contentPane.rowHeights = new int[]{5, 0, 5, 0};
-				gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-				gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+				gbl_contentPane.columnWidths = new int[] { 0, 0 };
+				gbl_contentPane.rowHeights = new int[] { 5, 0, 5, 0 };
+				gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+				gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 				panel.setLayout(gbl_contentPane);
-				
+
 				JLabel lblNombreGrupo = new JLabel(nombreGrupo);
 				lblNombreGrupo.setForeground(TEXT_COLOR_LIGHT);
 				GridBagConstraints gbc_lblEsaEss = new GridBagConstraints();
